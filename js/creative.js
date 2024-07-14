a42eb695f6214e02e5f525bd0153137a393bd3ca
AmirMohammadiKarbalaei <a.mohammadikarbalaei@gmail.com>
2024-07-14 10:25:30 +0100

    Added Particles.js

--- js/creative.js
+++ js/creative.js
@@ -111,3 +111,108 @@ function setEqualHeights() {
     content.style.height = maxHeight + "px";
   });
 }
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ["#ffffff", "#00d4ff", "#007bff", "#009286", "#ff5733", "#ffc300"],
+    },
+    shape: {
+      type: "polygon",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    opacity: {
+      value: 0.4,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.3,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 10,
+        size_min: 0.5,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 180,
+      color: "#ffffff",
+      opacity: 0.2,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 200,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 200,
+        size: 20,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
